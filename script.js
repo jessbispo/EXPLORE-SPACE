3f6415150d41de5504ad323de29c7eb843679738
jessica <jessicabispo.work@gmail.com>
2022-05-25 10:29:10 -0300

    finalizando css, adicionando responsividade

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ let btn = $(".btn").click(()=>{
  
 
 function buscarData() {
-    let dataDesejada = $(".dataDesejada").val() 
+    let dataDesejada = $(".dataDesejada").val()
+
     
-    $.ajax({
+    $.get({
         url: `https://api.nasa.gov/planetary/apod?api_key=5lVIJbkSYci40KMD082vyIGkrqTfrJCHGWptg70H&date=${dataDesejada}`,
         type: `GET`,
-        success: function(response){
-            console.log(response)
+        success: function(response){ 
     
             $("#img").html(`<img id="img" src="${response.hdurl}">`),
             $("#explicacao").text(`${response.explanation}`)
         },
-        error: function(error) {
-            if (dataDesejada <= "1995-06-16") {
+        error: function() {
+            if (dataDesejada < "1995-06-16") {
                 return alert("The first photo in the astronomy picture of the day was taken in 1995-06-16, please type a date above this day.")
             } //adicionar erro a datas inferiores a "1995-06-16"
         },
+          
 })
 }
 
@@ -29,15 +30,15 @@ let btnPrincipal = $('#btnPrincipal').click(()=>{ //ao clicar na pagina inicial,
         location.assign("apod.html")
 })
 
-let home = $('#home').click(()=>{ //ao clicar na pagina inicial, é redirecionado pra pagina inicial
+let home = $('#home').click(()=>{ //ao clicar, é redirecionado pra pagina inicial
     location.assign("index.html")
 })
 
-let linkedin = $('#linkedin').click(()=>{ //ao clicar na pagina inicial, é redirecionado pro meu linkedin
+let linkedin = $('#linkedin').click(()=>{ //ao clicar, é redirecionado pro meu linkedin
     location.assign("https://www.linkedin.com/in/j%C3%A9ssica-bispo-/")
 })
 
-let github = $('#github').click(()=>{ //ao clicar na pagina inicial, é redirecionado pro meu github
+let github = $('#github').click(()=>{ //ao clicar, é redirecionado pro meu github
     location.assign("https://github.com/jessbispo")
 })
 
